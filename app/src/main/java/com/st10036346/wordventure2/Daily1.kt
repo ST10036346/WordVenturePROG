deb4ff39723922820f812b0104af35af83c637ea
Ethan Pillay <ST10258615@vcconnect.edu.za>
2025-10-02 10:19:47 +0200

    Keyboard functionality

--- app/src/main/java/com/st10036346/wordventure2/Daily1.kt
+++ app/src/main/java/com/st10036346/wordventure2/Daily1.kt
@@ -24,6 +24,7 @@ class Daily1 : AppCompatActivity() {
 
         boardGrid = findViewById(R.id.boardGrid)
         setupBoard()
+        setupKeyboard()
 
 
 
@@ -54,7 +55,7 @@ class Daily1 : AppCompatActivity() {
 
     private fun onLetterPressed(letter: Char) {
         if (currentCol < cols) {
-            setTile(currentRow, currentCol, letter, Color.BLACK)
+            tiles[currentRow][currentCol]?.text = letter.toString()
             currentCol++
         }
     }
@@ -62,7 +63,7 @@ class Daily1 : AppCompatActivity() {
     private fun onBackPressedCustom() {
         if (currentCol > 0) {
             currentCol--
-            setTile(currentRow, currentCol, ' ', Color.WHITE)
+            tiles[currentRow][currentCol]?.text = ""
         }
     }
 
@@ -79,5 +80,34 @@ class Daily1 : AppCompatActivity() {
             text = if (letter == ' ') "" else letter.toString()
             setBackgroundColor(color)
         }
+    }
+
+    private fun setupKeyboard() {
+        // A map of Button IDs to the character they represent
+        val buttonIdToChar = mapOf(
+            R.id.button_q to 'Q', R.id.button_w to 'W', R.id.button_e to 'E',
+            R.id.button_r to 'R', R.id.button_t to 'T', R.id.button_y to 'Y',
+            R.id.button_u to 'U', R.id.button_i to 'I', R.id.button_o to 'O',
+            R.id.button_p to 'P', R.id.button_a to 'A', R.id.button_s to 'S',
+            R.id.button_d to 'D', R.id.button_f to 'F', R.id.button_g to 'G',
+            R.id.button_h to 'H', R.id.button_j to 'J', R.id.button_k to 'K',
+            R.id.button_l to 'L', R.id.button_z to 'Z', R.id.button_x to 'X',
+            R.id.button_c to 'C', R.id.button_v to 'V', R.id.button_b to 'B',
+            R.id.button_n to 'N', R.id.button_m to 'M'
+        )
+
+        // Loop through the map to set up each button
+        for ((buttonId, letter) in buttonIdToChar) {
+            findViewById<Button>(buttonId).setOnClickListener {
+                onLetterPressed(letter)
+            }
+        }
+
+        findViewById<Button>(R.id.button_enter).setOnClickListener { onEnterPressed() }
+        findViewById<Button>(R.id.button_delete).setOnClickListener { onBackPressedCustom()}
+
+        // You will also need to handle the Enter and Backspace buttons
+        // findViewById<Button>(R.id.button_enter).setOnClickListener { onEnterPressed() }
+        // findViewById<Button>(R.id.button_backspace).setOnClickListener { onBackPressedCustom() }
     }
 }
